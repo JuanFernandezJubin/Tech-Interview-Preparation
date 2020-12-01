941c5f92710f83d524ab23fc76e59983012420d1
Juan Ignacio <fernandezjubin@gmail.com>
2020-12-01 15:59:47 -0300

    04 done

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,12 +1,23 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//mdArraySum([1,2,3,4]); //should return 10
-//
-//mdArraySum([ [2,4] , [1], [4,2,1] ]); //should return 14
-//
-//mdArraySum([ 2, [3,4], 5, [-3, [6 , [ 4,5 ] ] ] ]); //should return 26
-//
 
-function mdArraySum(array) {
-  //Escribir la funcion
+//Alternativa 1
+const mdArraySum = (array) => {
+  // Iniciamos la suma en 0
+  let suma = 0;
+  // Iteramos sobre el arreglo
+  for (let i = 0; i < arr.length; i += 1) {
+      // si es un arreglo le sumamos lo que devuelve llamar otra vez la función sobre ese arreglo
+      if (Array.isArray(array[i])) suma += mdArraySum(array[i])
+      // si no le sumamos al total
+      else suma += array[i]  
+  }
+  // devolvemos el total
+  return suma;
 }
+//Complejidad Temporal : O(N) siendo N la cantidad de elemento del arreglo inclusive los arreglos internos
+
+//Complejidad Espacial : O(M) siendo M el nivel máximo de profundidad 
+
+//ALternativa 2 / in one line
+
+const mdArraySum2 = array =>    
+array.reduce((total, element) => total + (Array.isArray(element) ? mdArraySum2(element) : element), 0)
